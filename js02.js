eff6e9b85770c78c772bfc1eb2d564c361e27986
Mithun K. <mail2mithunkarthick@gmail.com>
2026-01-31 15:08:30 -0500

    third update

---
+++ js02.js
@@ -0,0 +1,20 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Mithunkarthick Parthiban 
+      Date: 1/31/2026
+
+      Filename: js02.js
+ */
+
+// set the form's default values
+function setupForm() {
+      document.getElementById("photoNum").value = 1;
+      document.getElementById("photoHrs").value = 2;
+      document.getElementById("makebook").checked = false;
+      document.getElementById("photoRights").checked = false;
+      document.getElementById("photoDist").value = 0;
+}
